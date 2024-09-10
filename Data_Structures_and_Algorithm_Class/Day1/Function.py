fa3f7bacc5496b177b9b68e25ab1e27bf7647dea
SeungWoo-L <seungwoo991123@gmail.com>
2024-09-10 16:36:34 +0900

    Fix Function Code, add print(), return in def

--- Data_Structures_and_Algorithm_Class/Day1/Function.py
+++ Data_Structures_and_Algorithm_Class/Day1/Function.py
@@ -3,17 +3,21 @@ def MAXMIN(sList):
         return None, None
     max, min = sList[0], sList[0]
     for i in range(0, len(sList)) :
-        if sList[i] > max : max = sList[i]
-        if sList[i] < min : min = sList[i]
+        if sList[i] > max: max = sList[i]
+        if sList[i] < min: min = sList[i]
+    return max, min
 
 sList = []
-print('임의의 정수를 입력하세요')
+print('임의의 정수를 입력하세요...')
 i = 0
 while True:
     num = input(f'sList[{i}] : ')
     if num == 'end' or num == '':
         break
     sList.append(int(num))
+    i+=1
     
-Max, Min = MAXMIN(sList)  
+max, min = MAXMIN(sList) 
+print(f'최댓값: {max}, 최솟값: {min}')
+ 
         
